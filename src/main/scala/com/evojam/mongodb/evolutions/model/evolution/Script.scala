18260bf0892c04c37232349cacc8d23ab6b12857
albert <albert@evojam.com>
2015-08-05 15:12:02 +0200

    Encode Evolution hash in base64
    Change-Id: I418afd19f04027d7a046f98d4a786afe55c2b662

--- src/main/scala/com/evojam/mongodb/evolutions/model/evolution/Script.scala
+++ src/main/scala/com/evojam/mongodb/evolutions/model/evolution/Script.scala
@@ -2,6 +2,7 @@ package com.evojam.mongodb.evolutions.model.evolution
 
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
+import java.util.Base64
 
 import com.kifi.macros.json
 
@@ -10,6 +11,7 @@ import com.kifi.macros.json
   require(value.nonEmpty, "value cannot be empty")
 
   def md5(): String =
-    new String(MessageDigest.getInstance("MD5")
-      .digest(value.trim.getBytes(StandardCharsets.UTF_8)))
+    Base64.getEncoder().encodeToString(
+      MessageDigest.getInstance("MD5")
+        .digest(value.trim.getBytes(StandardCharsets.UTF_8)))
 }
